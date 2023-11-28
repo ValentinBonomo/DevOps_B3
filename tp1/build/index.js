ef58964081878ad7c841b3ef82a43ad14bdb886c
ValentinBonomo <valentin.bonomo@ynov.com>
2023-11-28 11:48:50 +0100

    WIK-DPS-TP02

--- tp1/build/index.js
+++ tp1/build/index.js
@@ -1,22 +1,20 @@
 "use strict";
-const http = require("http");
-const host = 'localhost';
-const port = 8080;
-const requestListener = function (req, res) {
-    if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end("My first server!");
-    }
-    else if (req.url === '/ping') {
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+const port = process.env.PING_LISTEN_PORT || 3000;
+const server = http_1.default.createServer((req, res) => {
+    if (req.method === 'GET' && req.url === '/ping') {
         res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(req.headers, null, 2));
+        res.end(JSON.stringify(req.headers));
     }
     else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end();
     }
-};
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+});
+server.listen(port, () => {
+    console.log(`Serveur en écoute sur le port ${port}`);
 });
